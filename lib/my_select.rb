170ccdd7ecb319afd62129bcdcb783e6401f154c
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:54:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,5 +2,11 @@ def my_select(collection)
 new_arr = []
 counter = 0
 while counter<collection.length
-
+  if collection[counter] == yield
+    new_arr << collection[counter]
+    counter += 1
+  else
+    counter +=1
+  end
+  new_arr
 end
