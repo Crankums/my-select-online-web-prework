d49ec09168e3272169fe33bfa382720a7964f02d
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:49:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,3 @@
 def my_select(collection)
-  counter = 0
-  new_arr = []
-  collection.each do |i|
-    if i == yield
-      new_arr << i
-      new_arr
-    else
-      new_arr
-    end
-    end
+
 end
