d12713f17ea7b44859da99f370d8c47da23be0b9
Craig Donavin <crdonavin@gmail.com>
2019-04-06 02:31:39 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,8 @@
 def my_select(collection)
-  new_arr = []
   elem = 0
   while elem<collection.length
-    new_arr << collection[elem]
-    if yield new_arr[elem] == true
-      return new_arr[elem]
+    if collection[elem] == yield
+      collection[elem]
     end
       elem += 1
   end
